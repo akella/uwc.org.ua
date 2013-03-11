15d75b60055dbd6c70c9a1631c553b483bacd252
mirantel <magi.mirantel@gmail.com>
2013-03-11 20:36:50 +0200

    js

--- js/common.js
+++ js/common.js
@@ -6,4 +6,35 @@ $(document).ready(function() {
 		return false;
 	});
 
+	$(".js-member-login").click(function() {
+		$(".popup-wrap").fadeIn();
+		$(".popup").slideUp();
+		$(".js-popup_entry").slideDown();
+		return false;
+	});
+
+	$(".popup-wrap, .popup__close").click(function() {
+		$(".popup").slideUp();
+		$(".popup-wrap").fadeOut();
+		return false;
+	});
+
+	$(".js-btn_reg").click(function() {
+		$(".popup-wrap").fadeIn();
+		$(".popup").slideUp();
+		$(".js-popup_reg").slideDown();
+		return false;
+	});
+
+	$(".js-user-block").click(function() {
+		if($(this).hasClass('is-active')){
+			$(this).removeClass('is-active');
+			$('.user-block').slideUp();
+		}
+		else{
+			$(this).addClass('is-active');
+			$('.user-block').slideDown();
+		}
+		return false;
+	});
 });
