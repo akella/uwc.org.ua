8379398c8a635ee8d924c3ab798283ed60848be6
mirantel <magi.mirantel@gmail.com>
2013-03-11 14:18:44 +0200

    07_to-register

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
 
+	$('.tabs__nav a').click(function(){
+		if (!($(this).hasClass('is-active'))) $(this).addClass('is-active').siblings().removeClass('is-active')
+			.parents().find('.tabs__content').eq($(this).index()).fadeIn(150).siblings('.tabs__content').hide();
+		return false;
+	});
+
 });
