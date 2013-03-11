799c91c19fc154b41b517d79dd0a1dd42a22543d
mirantel <magi.mirantel@gmail.com>
2013-03-11 12:14:25 +0200

    social

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+	// function element exists
+	jQuery.fn.exists = function() {
+		return $(this).length;
+	}
 
+	// social-float
+	if ($('.social-float').exists()){
+		$(window).scroll(function(){
+			if ($(window).scrollTop() > 123) {
+				$('.social-float').css({'position':'fixed','top':'77px'});
+			}
+			else{
+				$('.social-float').css({'position':'absolute','top':'200px'});
+			}
+		});
+	}
 });
